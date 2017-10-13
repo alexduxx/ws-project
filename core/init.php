44f2e9e198565e132d02246c5a1db8767426ce1e
alexduxx <alexduxx@gmail.com>
2017-10-13 13:18:36 +0200

    created all the needed files for the LogIn
    at this point we have only the init file written

---
+++ core/init.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+
+
+$GLOBALS['config'] = array(
+    'mysql' => array(
+        'host'     => '127.0.0.1',
+        'username' => 'root',
+        'password' => '',
+        'db'       => 'ws-project'
+    ),
+    'remember'=> array(
+        'cookie_name' => 'hash',
+        'cookie_expiry' => 800000
+    ),
+    'session'=> array(
+        'session_name' => 'user'
+    )
+);
+
+
+// instead of writing 10 lines of coad to require all the files necessary 
+
+spl_autoload_register(function($class){
+    require_once 'classes/' . $class . '.php';
+});
+
+require_once 'functions/sanitize.php';
